a4f33adcf0905aca3028e284d0e2fcd5d946d66c
Thibault Riviere <thibault.riviere@scality.com>
2018-04-28 13:02:05 +0200

    ft(stats): add average function

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+extern crate num;
+
+pub fn average<'a, T: num::ToPrimitive >(t: &'a [T]) -> Option<f64>  {
+    if t.len() == 0 {
+        return None
+    }
+    Some(t.iter().map(|x| x.to_f64().unwrap()).sum::<f64>() / t.len() as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::average;
+
+    #[test]
+    fn test_average() {
+        assert_eq!(average(&[-2, -3, -4]), Some(-3.0));
+        assert_eq!(average(&[1.0, 1.0, 1.0]), Some(1.0));
+        assert_eq!(average(&[0, 0, 0]), Some(0.0));
+        let mut vec = vec!();
+        vec.push(42);
+        vec.clear();
+        assert_eq!(average(&vec), None);
+    }
+}
